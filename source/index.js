ecd8be078e75e392c7c0c11b6c24de547b576bac
fedeghe <fedeghe@gmail.com>
2019-03-24 01:25:27 +0100

    better setting sample

--- source/index.js
+++ source/index.js
@@ -1,64 +1,80 @@
+try{
+    const Cver = require('./cver'),
 
-const Cver = require('./cver'),
+        cver = new Cver(),
 
-    cver = new Cver(),
-
-    labels = {
-        header: {
-            title: 'i18n[Presentazione]'
-        },
-        body: {
-            component1: {
-                content: 'i18n[il mio contenuto]'
-            },
-            component2: {
-                content: 'i18n[un commento]'
-            }
-        }
-    },
-
-    config = {
-        outFolder: 'out',
-        translate: {
-            from: 'it',
-            to: 'en'
-        },
-        tpl: {
-            file: 'one',
-            style: 'dark',
+        labels = {
             header: {
-                name: 'header1',
-                data: {
-                    img: 'tryme/federico.jpg',
-                    title: labels.header.title,
-                    name: 'Federico Ghedina'
-                },
-                blocks: [{
-                    left: {
-                        name: 'headerInfo1',
-                        data: {
-                            content: 'contatto skype'
-                        }
-                    }
-                }]
+                title: 'i18n[Presentazione]'
             },
             body: {
-                blocks: [{
-                    name: 'component1',
-                    data: {
-                        content: labels.body.component1.content
-                    }
-                }, {
-                    name: 'component2',
-                    data: {
-                        content: labels.body.component2.content
-                    }
-                }]
-            },
-            footer: {
-                blocks: null
+                component1: {
+                    content: 'i18n[il mio contenuto]'
+                },
+                component2: {
+                    content: 'i18n[un commento]'
+                }
             }
-        }
-    };
-cver.setup(config);
-cver.print();
+        },
+
+        config = {
+            outFolder: 'out',
+            translate: {
+                from: 'it',
+                to: 'en'
+            },
+            tpl: {
+                name: 'one',
+                theme: 'dark',
+                data: {
+                    title: labels.header.title
+                },
+                // style: 'dark',
+                header: {
+                    data: {
+                        name: 'my name'
+                    },
+                    blocks: [{
+                        name: 'header1',
+                        data: {
+                            content: 'contatto skype',
+                            img: 'http://www.jmvc.org/fg.jpg',
+                            title: labels.header.title,
+                            name: 'Federico Ghedina'
+                        }
+                    }]
+                },
+                body: {
+                    data: {
+                        title: 'body title'
+                    },
+                    blocks: [{
+                        name: 'component1',
+                        data: {
+                            content: labels.body.component1.content
+                        }
+                    }, {
+                        name: 'component2',
+                        data: {
+                            content: labels.body.component2.content
+                        }
+                    }]
+                },
+                footer: {
+                    data: {
+                        name: 'i18n[il mio piede]'
+                    },
+                    blocks: [{
+                        name: 'footer1',
+                        data: {
+                            name: 'Erol'
+                        }
+                    }]
+                }
+            }
+        };
+    cver.setup(config);
+    cver.print();
+} catch (e) {
+    console.log(e);
+}
